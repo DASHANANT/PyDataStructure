ff1b1d135b4f56d42dc19a6d73f4f69fede3687d
Anant Dashpute <70088125+DASHANANT@users.noreply.github.com>
2021-11-10 19:19:36 +0530

    Update Add1.py
    New solution added

--- Arrays and Linked Lists/Add1.py
+++ Arrays and Linked Lists/Add1.py
@@ -20,5 +20,37 @@ def add_one(arr):
     last = total+1
     tab = [int(a) for a in str(last)]
     return tab
-    
- 
+
+# diffrent Solution
+
+def Add_One(arr):
+    borrow = 1
+    # Traverse in reverse direction starting from the end of the list
+    for i in range(len(arr), 0, -1):
+
+        # The "digit" denotes the updated Unit, Tens, and then Hunderd  position iteratively
+        digit = borrow + arr[i - 1]
+
+        """
+        The "borrow" will be carried to the next left digit
+        If the digit is between 0-9, borrrow will be 0.
+        If digit is 10, then the borrow will be 1.
+        """
+        # The "//" is a floor division operator
+        borrow = digit // 10
+
+        if borrow == 0:
+            # Update the arr[i - 1] with the updated digit, and quit the FOR loop.
+            arr[i - 1] = digit
+            break
+        else:
+            # Update the arr[i - 1] with the remainder of (digit % 10)
+            arr[i - 1] = digit % 10
+
+    # Prepend the final "borrow" to the original array.
+    arr = [borrow] + arr
+    position = 0
+    while arr[position] == 0:
+        position += 1
+
+    return arr[position:]
